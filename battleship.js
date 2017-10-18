2eb88f7410b31c9de89ef09a548b8effc9d18327
learn <learn@learn13.local>
2017-10-18 14:47:20 -0700

    added ships and hits

--- battleship.js
+++ battleship.js
@@ -1,21 +1,47 @@
+//Global Variables for Ship Creation
+// var rowIndex=event.target.id;
+// var colIndex=event.target.id;
+var link;
+var board = [
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+];
+var ship = 1;
+
 $(document).ready(function(){
     // Table
     for (indexR=0; indexR<10; indexR++){
-        $("#battleBoard").append("<tr>");
-    };
-    for (indexC=0; indexC<10; indexC++) {
-        $("tr").append("<td id='Col_" + indexC + "'>");
+        $("#battleBoard").append("<tr id='" + indexR + "'>");
+        for (indexC=0; indexC<10; indexC++) {
+            $("#battleBoard").append("<td id='" + indexR + " " +   indexC + "'>");
+        };
     };
 
     // Torpedo shot
     counter = 0;
 
-    $("td").on("click", function() {
-        $(this).addClass("torpedo");
+    $("td").on("click", function(event) {
+        link = event.target.id;
+        linkSplit = link.split(" ")
+        // console.log(linkSplit);
+        if (board[linkSplit[0]][linkSplit[1]] == 0){
+            $(this).addClass("torpedo");
+        } else {
+            $(this).addClass("hit");
+        }
         $(this).off("click");
         counter++;
-        if (counter > 25) {
-            alert("Oh no! You are out of torpedos! The enemy sinks your battleship!")
+        if (counter >= 25) {
+            alert("Oh no! You are out of torpedos! The enemy sinks your battleship!");
+            $("td").off("click");
         };
     });
 
@@ -24,4 +50,11 @@ $(document).ready(function(){
         $("#torpedoCounter").text(counter);
     });
 
+    //Creating Ships
+    for (placement = 0; placement < 5; placement++) {
+        board[Math.floor(Math.random()*10)][Math.floor(Math.random()*10)] = ship;
+    }
+
+
+//End of Document Ready
 });
